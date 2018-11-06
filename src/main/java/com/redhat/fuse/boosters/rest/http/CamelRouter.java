5d5bbd058874b59027a635ccc98f6212cce2a512
John Culleton <john.culleton@gmail.com>
2018-11-06 14:14:43 -0500

    Add kafka depedency

--- src/main/java/com/redhat/fuse/boosters/rest/http/CamelRouter.java
+++ src/main/java/com/redhat/fuse/boosters/rest/http/CamelRouter.java
@@ -1,7 +1,10 @@
 package com.redhat.fuse.boosters.rest.http;
 
+import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.kafka.KafkaComponent;
 import org.apache.camel.model.rest.RestBindingMode;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
@@ -11,9 +14,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class CamelRouter extends RouteBuilder {
 
+    @Autowired
+    private CamelContext camelContext;
 
     @Override
     public void configure() throws Exception {
+
+        KafkaComponent kafka = new KafkaComponent();
+        kafka.setBrokers("cis-kafka-cluster-kafka-bootstrap.amq-streams.svc:9092");
+        camelContext.addComponent("kafka", kafka);
 
         // @formatter:off
         restConfiguration()
@@ -28,15 +37,23 @@ public class CamelRouter extends RouteBuilder {
                 .apiContextRouteId("doc-api")
             .component("servlet")
             .bindingMode(RestBindingMode.json);
-        
-        rest("/greetings/").description("Greeting to {name}")
-            .get("/{name}").outType(Greetings.class)
-                .route().routeId("greeting-api")
-                .to("direct:greetingsImpl");
 
-        from("direct:greetingsImpl").description("Greetings REST service implementation route")
-            .streamCaching()
-            .to("bean:greetingsService?method=getGreetings");     
+        rest("/kafka-rest-proxy/pre-adjudication").description("")
+            .post().consumes("application/json")
+                .to("direct:pre-adjudication-kafka-proxy");
+        
+        //rest("/greetings/").description("Greeting to {name}")
+        //    .get("/{name}").outType(Greetings.class)
+        //        .route().routeId("greeting-api")
+        //        .to("direct:greetingsImpl");
+
+        //from("direct:greetingsImpl").description("Greetings REST service implementation route")
+        //    .streamCaching()
+        //    .to("bean:greetingsService?method=getGreetings");
+
+        from("direct:pre-adjudication-kafka-proxy").description("Places the pre-adjudication form message on a kafka topic")
+            .log("${body}")
+            .to("kafka:pre-adjudication-topic&groupId=pcs");
         // @formatter:on
     }
 
